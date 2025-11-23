8c62c54268154812173c8254e1070383aa72d254
vaishnavi <vaishnavicool1234@gmail.com>
2025-11-23 08:27:52 +0530

    vapi agent &feedback done

--- components/Agent.tsx
+++ components/Agent.tsx
@@ -5,6 +5,9 @@ import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 import { cn } from "@/lib/utils";
+import { vapi } from "@/lib/vapi.sdk";
+import { interviewer } from "@/constants";
+// import { createFeedback } from "@/lib/actions/general.action";
 
 enum CallStatus {
   INACTIVE = "INACTIVE",
@@ -28,27 +31,118 @@ const Agent = ({
 }: AgentProps) => {
   const router = useRouter();
   const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);
-  const [messages] = useState<SavedMessage[]>([]);
-  const [isSpeaking] = useState(false);
-  const [lastMessage] = useState<string>("");
+  const [messages, setMessages] = useState<SavedMessage[]>([]);
+  const [isSpeaking, setIsSpeaking] = useState(false);
+  const [lastMessage, setLastMessage] = useState<string>("");
 
   useEffect(() => {
-    if (callStatus === CallStatus.FINISHED) {
-      router.push("/");
-    }
-  }, [callStatus, router]);
+    const onCallStart = () => {
+      setCallStatus(CallStatus.ACTIVE);
+    };
 
-  const handleCall = () => {
+    const onCallEnd = () => {
+      setCallStatus(CallStatus.FINISHED);
+    };
+
+    const onMessage = (message: Message) => {
+      if (message.type === "transcript" && message.transcriptType === "final") {
+        const newMessage = { role: message.role, content: message.transcript };
+        setMessages((prev) => [...prev, newMessage]);
+      }
+    };
+
+    const onSpeechStart = () => {
+      console.log("speech start");
+      setIsSpeaking(true);
+    };
+
+    const onSpeechEnd = () => {
+      console.log("speech end");
+      setIsSpeaking(false);
+    };
+
+    const onError = (error: Error) => {
+      console.log("Error:", error);
+    };
+
+    vapi.on("call-start", onCallStart);
+    vapi.on("call-end", onCallEnd);
+    vapi.on("message", onMessage);
+    vapi.on("speech-start", onSpeechStart);
+    vapi.on("speech-end", onSpeechEnd);
+    vapi.on("error", onError);
+
+    return () => {
+      vapi.off("call-start", onCallStart);
+      vapi.off("call-end", onCallEnd);
+      vapi.off("message", onMessage);
+      vapi.off("speech-start", onSpeechStart);
+      vapi.off("speech-end", onSpeechEnd);
+      vapi.off("error", onError);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (messages.length > 0) {
+      setLastMessage(messages[messages.length - 1].content);
+    }
+
+    // const handleGenerateFeedback = async (messages: SavedMessage[]) => {
+    //   console.log("handleGenerateFeedback");
+
+    //   const { success, feedbackId: id } = await createFeedback({
+    //     interviewId: interviewId!,
+    //     userId: userId!,
+    //     transcript: messages,
+    //     feedbackId,
+    //   });
+
+    //   if (success && id) {
+    //     router.push(`/interview/${interviewId}/feedback`);
+    //   } else {
+    //     console.log("Error saving feedback");
+    //     router.push("/");
+    //   }
+    // };
+
+    // if (callStatus === CallStatus.FINISHED) {
+    //   if (type === "generate") {
+    //     router.push("/");
+    //   } else {
+    //     handleGenerateFeedback(messages);
+    //   }
+    // }
+  }, [messages, callStatus, feedbackId, interviewId, router, type, userId]);
+
+  const handleCall = async () => {
     setCallStatus(CallStatus.CONNECTING);
 
-    // Simply simulate call start
-    setTimeout(() => {
-      setCallStatus(CallStatus.ACTIVE);
-    }, 600);
+    if (type === "generate") {
+      await vapi.start(process.env.NEXT_PUBLIC_VAPI_WORKFLOW_ID!, {
+        variableValues: {
+          username: userName,
+          userid: userId,
+        },
+      });
+    } else {
+      let formattedQuestions = "";
+      if (questions) {
+        formattedQuestions = questions
+          .map((question) => `- ${question}`)
+          .join("\n");
+      }
+
+      await vapi.start(interviewer, {
+        variableValues: {
+          questions: formattedQuestions,
+        },
+      });
+    }
   };
 
   const handleDisconnect = () => {
     setCallStatus(CallStatus.FINISHED);
+    vapi.stop();
   };
 
   return (
@@ -84,17 +178,25 @@ const Agent = ({
         </div>
       </div>
 
-      {lastMessage && (
+      {messages.length > 0 && (
         <div className="transcript-border">
           <div className="transcript">
-            <p className={cn("animate-fadeIn opacity-100")}>{lastMessage}</p>
+            <p
+              key={lastMessage}
+              className={cn(
+                "transition-opacity duration-500 opacity-0",
+                "animate-fadeIn opacity-100"
+              )}
+            >
+              {lastMessage}
+            </p>
           </div>
         </div>
       )}
 
       <div className="w-full flex justify-center">
         {callStatus !== "ACTIVE" ? (
-          <button className="relative btn-call" onClick={handleCall}>
+          <button className="relative btn-call" onClick={() => handleCall()}>
             <span
               className={cn(
                 "absolute animate-ping rounded-full opacity-75",
@@ -109,7 +211,7 @@ const Agent = ({
             </span>
           </button>
         ) : (
-          <button className="btn-disconnect" onClick={handleDisconnect}>
+          <button className="btn-disconnect" onClick={() => handleDisconnect()}>
             End
           </button>
         )}
--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 import Image from "next/image";
 
 import { Button } from "./ui/button";
-import DisplayTechIcons from "./DisplayTechIcon";
+import DisplayTechIcons from "@/components/DisplayTechIcon";
+
 import { cn, getRandomInterviewCover } from "@/lib/utils";
+import { getFeedbackByInterviewId } from "@/lib/actions/general.action";
 
 const InterviewCard = async ({
   interviewId,
@@ -14,6 +16,14 @@ const InterviewCard = async ({
   techstack,
   createdAt,
 }: InterviewCardProps) => {
+  const feedback =
+    userId && interviewId
+      ? await getFeedbackByInterviewId({
+          interviewId,
+          userId,
+        })
+      : null;
+
   const normalizedType = /mix/gi.test(type) ? "Mixed" : type;
 
   const badgeColor =
@@ -23,7 +33,9 @@ const InterviewCard = async ({
       Technical: "bg-light-800",
     }[normalizedType] || "bg-light-600";
 
-  const formattedDate = dayjs(createdAt || Date.now()).format("MMM D, YYYY");
+  const formattedDate = dayjs(
+    feedback?.createdAt || createdAt || Date.now()
+  ).format("MMM D, YYYY");
 
   return (
     <div className="card-border w-[360px] max-sm:w-full min-h-96">
@@ -36,7 +48,7 @@ const InterviewCard = async ({
               badgeColor
             )}
           >
-            <p className="badge-text">{normalizedType}</p>
+            <p className="badge-text ">{normalizedType}</p>
           </div>
 
           {/* Cover Image */}
@@ -48,26 +60,31 @@ const InterviewCard = async ({
             className="rounded-full object-fit size-[90px]"
           />
 
-          {/* Role */}
+          {/* Interview Role */}
           <h3 className="mt-5 capitalize">{role} Interview</h3>
 
-          {/* Date */}
+          {/* Date & Score */}
           <div className="flex flex-row gap-5 mt-3">
             <div className="flex flex-row gap-2">
-              <Image src="/calendar.svg" width={22} height={22} alt="calendar" />
+              <Image
+                src="/calendar.svg"
+                width={22}
+                height={22}
+                alt="calendar"
+              />
               <p>{formattedDate}</p>
             </div>
 
-            {/* Static Score placeholder */}
             <div className="flex flex-row gap-2 items-center">
               <Image src="/star.svg" width={22} height={22} alt="star" />
-              <p>--/100</p>
+              <p>{feedback?.totalScore || "---"}/100</p>
             </div>
           </div>
 
-          {/* Placeholder text */}
+          {/* Feedback or Placeholder Text */}
           <p className="line-clamp-2 mt-5">
-            You haven't taken this interview yet. Take it now to improve your skills.
+            {feedback?.finalAssessment ||
+              "You haven't taken this interview yet. Take it now to improve your skills."}
           </p>
         </div>
 
@@ -75,8 +92,14 @@ const InterviewCard = async ({
           <DisplayTechIcons techStack={techstack} />
 
           <Button className="btn-primary">
-            <Link href={`/interview/${interviewId}`}>
-              View Interview
+            <Link
+              href={
+                feedback
+                  ? `/interview/${interviewId}/feedback`
+                  : `/interview/${interviewId}`
+              }
+            >
+              {feedback ? "Check Feedback" : "View Interview"}
             </Link>
           </Button>
         </div>
